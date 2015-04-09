e90219f342ecea1066f3681a2b1d6d62fde1845b
dinesh <dinesh121991@gmail.com>
2015-04-09 14:48:54 +0200

    Older layout conf,resource selection important papers

--- Conf/Mine/generate-socket-conf.c
+++ Conf/Mine/generate-socket-conf.c
@@ -3,10 +3,10 @@
 int main( int argc, char*argv[] )
 {
 	int i=0,j=0;
-	for( i=0 ; i< 80 ; i++ ) 
+	for( i=0 ; i<= 81 ; i++ ) 
 	{
 		j= 2*i;
-		printf("Entity=socket%d type=socket JobId=0 NumSumCores=0 FreeSumCores=0 AllocatedSumCores=0  ForWhatPurposePriority=0 BitmapPosition=0 Selected=0 AllocatedSocketCount=0 socketcount=1 Enclosed=core[%d-%d] \n",i,j,j+1);
+		printf("Entity=socket%d type=socket NumSumCores=0 SelectedSumCores=0 AllocatedSumCores=0  BitmapPosition=0 SelectedSocketCount=0 AllocatedSocketCount=0 socketcount=1 Enclosed=core[%d-%d] \n",i,j,j+1);
 	}
 	return 0;
 }
