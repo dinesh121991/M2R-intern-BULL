f2acf1e475ae589d3f884fe0ba07e79f33abe785
Dineshkumar Rajagopal <dinesh121991@gmail.com>
2015-05-27 10:10:59 +0200

    PE files and powerHPC papers OpenMp reference

---
+++ Conf/Experiment-3000-virtual-nodes/node-conf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int nb_nodes=3000,port=17000,i=0,j=0;
+	for(i=0,j=0 ; i < 1000 ; i++,j++ )
+	{
+		printf("NodeName=virtual%d NodeHostName=mo66 NodeAddr=mo66 Port=%d\n",i,(port+j));
+	}
+	for(i=1000,j=0 ; i<2000; i++,j++ )
+	{
+		printf("NodeName=virtual%d NodeHostName=mo67 NodeAddr=mo67 Port=%d\n",i,(port+j));
+	}
+	for(i=2000,j=0 ; i<3000 ; i++,j++ )
+	{
+		printf("NodeName=virtual%d NodeHostName=mo68 NodeAddr=mo68 Port=%d\n",i,(port+j));
+	}
+	return 0;
+}
